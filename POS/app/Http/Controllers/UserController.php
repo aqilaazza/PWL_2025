a3c256321fc640a0a7dd9666255b7b660638d918
aqilaazza <aqilanurazza123@gmail.com>
2025-03-01 14:16:22 +0700

    Implementasi Halaman User

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function show($id, $name) // menerima 2 parameter
+    {
+        return view('user.profile', compact('id', 'name')); // mengirim variable id dan name ke dalam view
+    }
 }
