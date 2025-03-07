b329d148b0f1273092af03c8d81120ed09139ae3
aqilaazza <aqilanurazza123@gmail.com>
2025-03-08 03:43:52 +0700

    Praktikum-2.5

--- PWL_WEEK4/PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_WEEK4/PWL_POS/app/Http/Controllers/UserController.php
@@ -11,29 +11,26 @@ class UserController extends Controller
     public function index()
     {
         $user = UserModel::create([
-            'username' => 'manager55',
-            'nama' => 'Manager55',
+            'username' => 'manager11',
+            'nama' => 'Manager11',
             'password' => Hash::make('12345'),
             'level_id' => 2,
+            //Data berhasil disimpan ke database dengan username manager11
         ]);
 
-        $user->username = 'manager56';
-
-        $user->isDirty(); // true
-        $user->isDirty('username'); // true
-        $user->isDirty('nama'); // false
-        $user->isDirty(['nama', 'username']); // true
-
-        $user->isClean(); // false
-        $user->isClean('username'); // false
-        $user->isClean('nama'); // true
-        $user->isClean(['nama', 'username']); // false
+        $user->username = 'manager12';
+        // Sekarang ada perubahan, tetapi belum disimpan ke database.
 
         $user->save();
+        //Perubahan username sudah tersimpan di database.
 
-        $user->isDirty(); // false
-        $user->isClean(); // true
-        dd($user->isDirty());
+        $user->wasChanged(); //true (Mengembalikan true karena ada perubahan (username))
+        $user->wasChanged('username'); //true (Mengembalikan true karena username memang berubah)
+        $user->wasChanged(['username', 'level_id']);//true (Mengembalikan true karena username berubah (meskipun level_id tidak berubah, tapi karena salah satunya berubah, hasilnya tetap true))
+        $user->wasChanged('nama');//false (Mengembalikan false karena nama tidak berubah)
+        dd($user->wasChanged(['nama', 'username']));//true (Mengembalikan true karena username berubah (meskipun nama tidak berubah, karena salah satunya berubah, hasilnya tetap true))
+
+        
     }
 }
 
