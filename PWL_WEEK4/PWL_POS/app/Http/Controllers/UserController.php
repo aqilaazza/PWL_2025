b116355dfe16e6af2edbb6bc95c15ce9f1c025ab
aqilaazza <aqilanurazza123@gmail.com>
2025-03-08 00:55:35 +0700

    Praktikum-2.4

--- PWL_WEEK4/PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_WEEK4/PWL_POS/app/Http/Controllers/UserController.php
@@ -10,8 +10,14 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('level_id', 2)->count();
-        //dd($user);
+        $user = UserModel:: firstOrCreate(
+        [
+            'username' => 'manager22', 
+            'nama' => 'Manager Dua Dua',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ],
+    );
         return view('user', ['data' => $user]);
     }
 }
