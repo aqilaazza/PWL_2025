262058c1b3f9baef37382683fe51e858ce2791ea
aqilaazza <aqilanurazza123@gmail.com>
2025-03-22 04:06:02 +0700

    Tugas-1 web layout m_level

--- PWL_WEEK5.2/PWL_POS/app/Models/LevelModel.php
+++ PWL_WEEK5.2/PWL_POS/app/Models/LevelModel.php
@@ -11,7 +11,7 @@ class LevelModel extends Model
 {
     protected $table = 'm_level';
     protected $primaryKey = 'level_id';
-    protected $fillable = ['level_kode', 'level_name']; //Foreign key
+    protected $fillable = ['level_kode', 'level_nama']; //Foreign key
 
     public function users(): HasMany
     {
