5bcf448611189df22903b8c5f5edd585c3227e77
aqilaazza <aqilanurazza123@gmail.com>
2025-04-08 19:52:20 +0700

    Change welcome message

--- POS/resources/views/welcome.blade.php
+++ POS/resources/views/welcome.blade.php
@@ -4,11 +4,13 @@
 
 <div class="card">
     <div class="card-header">
-        <h3 class="card-title">Halo, apa kabar!!!</h3>
+        <h3 class="card-title">Hai, Selamat Datang di POS! 👋</h3>
         <div class="card-tools">x</div>
     </div>
     <div class="card-body">
-        Selamat datang semua, ini adalah halaman utama dari aplikasi ini.
+        Welcome back, {{ Auth::user()->username }}!  
+        Semangat kerja hari ini ya 💪  
+        Yuk, mulai aktivitasmu di halaman utama ini, {{ Auth::user()->nama }}.
     </div>
 </div>
 
