8f85aeba192849d0a94d3a72e2c8fde265ce121e
aqilaazza <aqilanurazza123@gmail.com>
2025-04-09 12:18:58 +0700

    Detailed revision

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -238,8 +238,8 @@ class UserController extends Controller
         }
         
         // Ambil data user dalam bentuk JSON untuk DataTables
-        public function list(Request $request)
-        {
+    public function list(Request $request)
+    {
             $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
         ->with('level');
 
@@ -251,17 +251,17 @@ class UserController extends Controller
     return DataTables::of($users)
         ->addIndexColumn() // Menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
         ->addColumn('aksi', function ($user) { // Menambahkan kolom aksi
-            /*
+            
             $btn = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn-sm">Detail</a> ';
-            $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> ';
-            $btn .= '<form class="d-inline-block" method="POST" action="'. url('/user/'.$user->user_id).'">'
-                . csrf_field() . method_field('DELETE') . 
-                '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button>
-                </form>';
-            */
+            // $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> ';
+            // $btn .= '<form class="d-inline-block" method="POST" action="'. url('/user/'.$user->user_id).'">'
+            //     . csrf_field() . method_field('DELETE') . 
+            //     '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button>
+            //     </form>';
+            
 
-            $btn = '<button onclick="modalAction(\''.url('/user/' . $user->user_id . '/show_ajax').'\')" 
-                    class="btn btn-info btn-sm">Detail</button> ';
+            // $btn = '<button onclick="modalAction(\''.url('/user/' . $user->user_id . '/show_ajax').'\')" 
+            //         class="btn btn-info btn-sm">Detail</button> ';
 
             $btn .= '<button onclick="modalAction(\''.url('/user/' . $user->user_id . '/edit_ajax').'\')" 
                     class="btn btn-warning btn-sm">Edit</button> ';
